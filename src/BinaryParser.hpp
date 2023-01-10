1e5f8285c6d0e6690fdcaace6aa949f33e7e0473
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2023-01-10 21:53:46 +0100

    Refactor: clean up header.

--- src/BinaryParser.hpp
+++ src/BinaryParser.hpp
@@ -35,16 +35,6 @@ class BinaryParser : public Parser
 public:
     BinaryParser();
 
-    void parse( std::istream&                                     stream,
-                const std::vector<Token>&                         tokens,
-                std::vector<std::pair<std::string, RoffScalar>>&  scalarValues,
-                std::vector<std::pair<std::string, Token::Kind>>& arrayTypes,
-                std::map<std::string, std::pair<long, long>>&     arrayInfo ) const;
-
-    std::pair<std::string, RoffScalar> parseSimpleType( std::vector<Token>::const_iterator& it,
-                                                        const std::string&                  tagGroupName,
-                                                        std::istream&                       stream ) const;
-
     std::vector<std::string> parseStringArray( const std::vector<Token>& tokens,
                                                std::istream&             stream,
                                                long                      startIndex,
@@ -69,8 +59,6 @@ public:
                                       std::istream&             stream,
                                       long                      startIndex,
                                       long                      arrayLength ) const override;
-
-    static bool isSimpleType( Token::Kind kind );
 
     std::string   parseString( const Token& token, std::istream& stream ) const override;
     int           parseInt( const Token& token, std::istream& stream ) const override;
